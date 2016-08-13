fdc35e55b1d050570b204f7aebacbb266d2f7f15
youwenyu <yodyu@qq.com>
2016-08-13 19:05:37 +0800

    371

---
+++ 371. Sum of Two Integers.java
@@ -0,0 +1,20 @@
+/* 371. Sum of Two Integers
+ * s = a ^ b 在忽略进位的情况下a与b的和
+ * c = a & b 各个位的进位
+ * a b的和等于s c的和
+*/
+public class Solution {
+
+	public int getSum(int a, int b) {
+		int c = a & b;
+		int s = a ^ b;
+		while (c != 0) {
+			c = c << 1;
+			a = s;
+			b = c;
+			s = a ^ b;
+			c = a & b;
+		}
+		return s;
+	}
+}
