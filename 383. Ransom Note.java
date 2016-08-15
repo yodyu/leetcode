314ac837dcf38d6e131b013e198304cf37523122
youwenyu <yodyu@qq.com>
2016-08-15 20:07:15 +0800

    383

---
+++ 383. Ransom Note.java
@@ -0,0 +1,22 @@
+/* 383. Ransom Note
+ * Hash
+ * 小写字母的ASCII：97-122
+ */
+public class Solution {
+
+	public boolean canConstruct(String ransomNote, String magazine) {
+		int[] letter_count = new int[123];
+        for (int i=0;i<magazine.length();i++){
+        	letter_count[magazine.charAt(i)] ++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+        	if(letter_count[ransomNote.charAt(i)]>0){
+        		letter_count[ransomNote.charAt(i)]--;
+        	}
+        	else{
+        		return false;
+        	}
+        }
+        return true;
+    }
+}
